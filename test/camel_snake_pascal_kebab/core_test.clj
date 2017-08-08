56a7b360ccd940d5c01aa6b269cff628a9fcdf5a
Alvaro Garcia <derecerca@gmail.com>
2017-08-08 19:32:33 +0200

    add project with dependencies

---
+++ test/camel_snake_pascal_kebab/core_test.clj
@@ -0,0 +1,7 @@
+(ns camel-snake-pascal-kebab.core-test
+  (:require [clojure.test :refer :all]
+            [camel-snake-pascal-kebab.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
