166e084755f0428957509a08e039823b70e819cb
Joaquin Caro <me@joaquin-caro.es>
2017-08-09 23:11:48 +0200

    Green: from camel to kebab case

--- src/camel_snake_pascal_kebab/core.clj
+++ src/camel_snake_pascal_kebab/core.clj
@@ -1,24 +1,25 @@
 (ns camel-snake-pascal-kebab.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
 
 
 (defn to-camel-case
   [words]
   (let [rest-words (rest words)
-        rest-words-formatted (apply str (map clojure.string/capitalize rest-words))]
+        rest-words-formatted (apply str (map str/capitalize rest-words))]
     (keyword (str (first words) rest-words-formatted))))
 
 (defn to-pascal-case
   [words]
-  (keyword (apply str (map clojure.string/capitalize words))))
+  (keyword (apply str (map str/capitalize words))))
 
 (defn to-snake-case
   [words]
-  (keyword (clojure.string/join "_" words)))
+  (keyword (str/join "_" words)))
 
 (defn to-kebab-case
   [words]
-  (keyword (clojure.string/join "-" words)))
+  (keyword (str/join "-" (map str/lower-case words))))
 
 (def formats
   {:camel-case to-camel-case
@@ -26,7 +27,11 @@
    :snake-case to-snake-case
   :kebab-case to-kebab-case})
 
+(defn split-words
+  [input]
+  (str/split (name input) #"-|_|(?=[A-Z])"))
+
 (defn format
   [input _ format-to]
-  (let [words (clojure.string/split (name input) #"-")]
+  (let [words (split-words input)]
         ((get formats format-to) words)))
--- test/camel_snake_pascal_kebab/core_test.clj
+++ test/camel_snake_pascal_kebab/core_test.clj
@@ -7,16 +7,21 @@
 (facts
   "converting in different cases"
   (fact
-    "from kebab case"
+    "from kebab to camel case"
     (format :hello-koko :using :camel-case) => :helloKoko
     (format :hello-koko-pepe :using :camel-case) => :helloKokoPepe
-    (format :hello-pepe :using :camel-case) => :helloPepe
-    )
+    (format :hello-pepe :using :camel-case) => :helloPepe)
+
   (fact
     "from kebab to snake case"
     (format :hello-koko :using :snake-case) => :hello_koko
     (format :hello-koko-pepe :using :snake-case) => :hello_koko_pepe)
+
   (fact
     "from kebab to kebab case"
     (format :hello-koko :using :kebab-case) => :hello-koko
-    (format :hello-koko-pepe :using :kebab-case) => :hello-koko-pepe))
+    (format :hello-koko-pepe :using :kebab-case) => :hello-koko-pepe)
+
+  (fact
+    "from camel to kebab case"
+    (format :helloKoko :using :kebab-case) => :hello-koko))
