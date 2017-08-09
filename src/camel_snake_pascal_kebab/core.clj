673b5b52faaff7876460136774b7a0e30df1e955
Joaquin Caro <me@joaquin-caro.es>
2017-08-09 08:29:17 +0200

    Split test and productive code

--- src/camel_snake_pascal_kebab/core.clj
+++ src/camel_snake_pascal_kebab/core.clj
@@ -1,7 +1,28 @@
 (ns camel-snake-pascal-kebab.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn format-word [word]
+      (str (Character/toUpperCase (first word))
+           (apply str (rest word))))
+
+(defn to-camel-case [text]
+      (let [words (clojure.string/split (name text) #"-")
+            first-word (first words)
+            rest-words (rest words)
+            rest-words-formatted (apply str (map format-word rest-words))]
+           (keyword (str
+                      first-word
+                      rest-words-formatted))))
+(defn to-pascal-case [text]
+      (let [words (clojure.string/split (name text) #"-")
+            words-formatted (apply str (map format-word words))]
+           (keyword words-formatted))
+      )
+
+(defn format
+      [input _ format-to]
+      (if (= format-to :camel-case)
+        (to-camel-case input)
+        (if (= format-to :pascal-case)
+          (to-pascal-case input)
+          :hello_koko)))
