1c16879ddf094cfcd10fcce72507d3076e5d7c6d
Joaquin Caro <me@joaquin-caro.es>
2017-08-09 22:39:50 +0200

    Refactor: format functions gets names splited

--- src/camel_snake_pascal_kebab/core.clj
+++ src/camel_snake_pascal_kebab/core.clj
@@ -2,22 +2,18 @@
   (:gen-class))
 
 
-
-(defn to-camel-case [text]
-      (let [words (clojure.string/split (name text) #"-")
-            first-word (first words)
+(defn to-camel-case [words]
+      (let [first-word (first words)
             rest-words (rest words)
             rest-words-formatted (apply str (map clojure.string/capitalize rest-words))]
-           (keyword (str
-                      first-word
-                      rest-words-formatted))))
-(defn to-pascal-case [text]
-      (let [words (clojure.string/split (name text) #"-")
-            words-formatted (apply str (map clojure.string/capitalize words))]
+           (keyword (str first-word rest-words-formatted))))
+
+(defn to-pascal-case [words]
+      (let [words-formatted (apply str (map clojure.string/capitalize words))]
            (keyword words-formatted)))
 
 (defn to-snake-case
-      [text]
+      [words]
       :hello_koko)
 
 (def formats
@@ -26,5 +22,6 @@
    :snake-case to-snake-case})
 
 (defn format
-      [input _ format-to]
-      ((get formats format-to) input))
+  [input _ format-to]
+  (let [words (clojure.string/split (name input) #"-")]
+        ((get formats format-to) words)))
